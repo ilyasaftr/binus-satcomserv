9019516a96c00e5a5c398523c8c60643c5a903f4
ilyasaftr <ilyasarecord@gmail.com>
2023-03-14 08:20:11 +0700

    add klifonara

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const connectDB = require('./config/database');
+const updateInstagramTarget = require('./cmd/addManualTarget');
 const taskGetInstagramPost = require('./tasks/getInstagramPost');
 const taskCheckInstagramPost = require('./tasks/checkInstagramPost');
 const taskCreateInstagramPost = require('./tasks/createInstagramPost');
@@ -8,7 +9,7 @@ const instagramClient = require('./config/instagram');
 async function main() {
   try {
     await connectDB();
-
+    await updateInstagramTarget();
     // trying login to instagram
     try {
       const image_data = {
